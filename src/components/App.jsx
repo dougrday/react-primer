f45a7a49be33c111e33329a2d1123d9767aa1cb3
Doug Day <dougrday@gmail.com>
2016-03-22 10:26:31 -0600

    Use full ES6 classes for Components, for ease-of-understanding

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
 
 const styles = {
@@ -12,22 +12,23 @@ const styles = {
 };
 
 // NOTE: you can think of this page as a 'skin' used by react-router.
-const App = (props) => {
-    return (
-        <div className="row" style={styles.body}>
-            {/* A simple responsive design to center an area of the page */}
-            <div className="col-xs-offset-0 col-xs-12 col-sm-offset-1 col-sm-10 col-md-offset-2 col-md-8" style={styles.center}>
-                <IndexLink to="/">Home</IndexLink> | <Link to="/About">About</Link> | <Link to="/badlink">Bad Link (404)</Link>
-                <div style={{marginTop:"1em"}}>
-                    {props.children}
+export default class App extends Component
+{
+    static propTypes = {
+        children: PropTypes.element
+    };
+
+    render() {
+        return (
+            <div className="row" style={styles.body}>
+                {/* A simple responsive design to center an area of the page */}
+                <div className="col-xs-offset-0 col-xs-12 col-sm-offset-1 col-sm-10 col-md-offset-2 col-md-8" style={styles.center}>
+                    <IndexLink to="/">Home</IndexLink> | <Link to="/About">About</Link> | <Link to="/badlink">Bad Link (404)</Link>
+                    <div style={{marginTop:"1em"}}>
+                        {this.props.children}
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-};
-
-App.propTypes = {
-    children: PropTypes.element
-};
-
-export default App;
+        );
+    }
+}
--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
 
-const NotFoundPage = () => {
-    return (
-        <div>
-            <h4>404 Page Not Found</h4>
-            <Link to="/"> Go back to homepage </Link>
-        </div>
-    );
-};
-
-export default NotFoundPage;
+export default class NotFoundPage extends Component
+{
+    render() {
+        return (
+            <div>
+                <h4>404 Page Not Found</h4>
+                <Link to="/"> Go back to homepage </Link>
+            </div>
+        );
+    }
+}
